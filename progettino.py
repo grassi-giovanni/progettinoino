70b173849957b46de223f93a3f8db9e662802f6c
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-11-14 08:29:04 +0000

    4

--- progettino.py
+++ progettino.py
@@ -8,3 +8,13 @@ def visualizza_listaSpesa(listaSpesa):
     print("lista della spesa")
     for i in range(len(listaSpesa)):
         print(f"{i + 1}. {listaSpesa[i]}")
+
+def rimuovi_daListaSpesa(listaSpesa):
+    prodotto = input ("inserisci il prodotto da rimuovere dalla lista:")
+    for i in range(len(listaSpesa)):
+        if listaSpesa[i] == prodotto:
+            listaSpesa.pop(listaSpesa[i])
+
+        
+    
+    
