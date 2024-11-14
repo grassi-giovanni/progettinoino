13ebca783c7833d4bf3036ec65f5331412763702
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-11-14 08:25:09 +0000

    3

--- progettino.py
+++ progettino.py
@@ -3,3 +3,8 @@ listaSpesa = []
 def riempi_listaSpesa(listaSpesa):
     prodotto = input("inserire prodotto:")
     listaSpesa.append(prodotto)
+
+def visualizza_listaSpesa(listaSpesa):
+    print("lista della spesa")
+    for i in range(len(listaSpesa)):
+        print(f"{i + 1}. {listaSpesa[i]}")
