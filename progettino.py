80edc0f6e5db789d2e90d4f4ff79444082dbf01f
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-11-15 10:47:39 +0000

    versioneDefinitiva

--- progettino.py
+++ progettino.py
@@ -1,5 +1,6 @@
 listaSpesa = []
 
+#ciclo while che tramite input inserisce un elemento
 def riempi_listaSpesa(listaSpesa):
     while True:
         prodotto = input("inserire prodotto (per uscire digita /esc):")
@@ -7,25 +8,33 @@ def riempi_listaSpesa(listaSpesa):
             break
         else:listaSpesa.append(prodotto)
         
+#tramite ciclo for (con la lunghezza della lista) stampa la lista formattata
 def visualizza_listaSpesa(listaSpesa):
-    print("lista della spesa")
+    print("---lista della spesa---")
     for i in range(len(listaSpesa)):
-        print(f"{i + 1}. {listaSpesa[i]}")
+        print(f"{i + 1}. {listaSpesa[i]}")#stampa la lista con una formattazzione più "piacevole"(mette il numero d'indice davanti all'elemento e va a capo)
+    print("-----------------------")
 
+# ciclo che continua finché l'utente non inserisce '/esc'
+# se il prodotto è presente nella lista, viene rimosso
+# la lista viene poi ristampata per visualizzare lo stato aggiornato
 def rimuovi_daListaSpesa(listaSpesa):
     while True:
         prodotto = input ("inserisci il prodotto da rimuovere dalla lista (per uscire digita /esc):")
         if prodotto == "/esc":
             break
         else: 
-            for i in range(len(listaSpesa)):
-                if listaSpesa[i] == prodotto:
-                    listaSpesa.pop(listaSpesa[i])
+            while prodotto in listaSpesa:
+                listaSpesa.remove(prodotto)
+                print("---lista della spesa---")
+                for i in range(len(listaSpesa)):
+                    print(f"{i + 1}. {listaSpesa[i]}")
+                print("-----------------------")
     
     
     
 
-        
+#classico menu con ciclo while       
 def menu(listaSpesa):
     while True:
         print("Agisci sulla lista della spesa")    
@@ -39,4 +48,4 @@ def menu(listaSpesa):
 
 menu(listaSpesa)
 
-#MANCA ULTIMO COMMIT PER SISTEMARE LA RIMOZIONE E COMMENTI#
+
