01e21284790de3b6269b36b6d1bfce4ba97bea37
grassi-giovanni <grassi.giovanni@iisgalvanimi.edu.it>
2024-11-19 08:26:31 +0000

    4(funziona ancora tutto)

--- progettino2/app.py
+++ progettino2/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+
 #inizializza l'app Flask
 app = Flask(__name__)
+
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
+    return render_template('index.html')
+
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
